afe802a829182dddf60c68dcf7723560353b74d3
Johannes Hofmeister <github@account.cessor.de>
2018-11-09 23:38:56 +0100

    Version for a small bugfix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'url': 'https://github.com/cessor/kazookid',
     'download_url': 'https://github.com/cessor/kazookid',
     'author_email': 'py_kazookid@spam.cessor.de',
-    'version': '1.3',
+    'version': '1.3.1',
     'tests_require': ['nose'],
     'packages': ['kazookid'],
     'scripts': [],
